4ad482e45be53f9536b3236294c7887db9275b53
Tom <whynotavailable@protonmail.com>
2019-04-25 21:57:56 -0400

    Updated example

--- example/example.go
+++ example/example.go
@@ -9,17 +9,20 @@ import (
 	"strings"
 )
 
-var targetRange *net.IPNet
-
 func getClientIP(r *http.Request) net.IP {
 	return net.ParseIP(r.Header.Get("TrueClient-Ip"))
 }
 
 func middleware(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	_, target, err := net.ParseCIDR("128.0.0.1/24")
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		ip := getClientIP(r)
-
-		if !targetRange.Contains(ip) {
+		if !target.Contains(ip) {
 			http.Error(w, "Maintenence", http.StatusServiceUnavailable)
 			return
 		}
@@ -38,25 +41,23 @@ func mainForwarder(req *http.Request) proxy.ForwarderOptions {
 	}
 }
 
-func main() {
-	// cidr testing
-	_, network, err := net.ParseCIDR("128.0.0.34/24")
-	if err == nil {
-		targetRange = network
-	}
-
-	// actual main
-	http.HandleFunc("/proxy/", middleware(proxy.Forwarder(mainForwarder)))
-
-	http.HandleFunc("/userid", func(w http.ResponseWriter, r *http.Request) {
-		b, _ := json.Marshal(userIDMessage{
-			"john",
-		})
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
+func userGetter(w http.ResponseWriter, r *http.Request) {
+	b, _ := json.Marshal(userIDMessage{
+		"john",
 	})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
 
-	http.HandleFunc("/", middleware(proxy.StaticFileHoster("./static", "/example.txt")))
+func main() {
+	// actual main
+	http.HandleFunc("/proxy/", proxy.Forwarder(mainForwarder))
+
+	http.HandleFunc("/userid", middleware(userGetter))
+
+	http.HandleFunc("/", proxy.StaticFileHoster("./static", "/example.txt"))
+
 	log.Println("Starting Server")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
